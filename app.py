7724c0a044051bff31896519fbb29676d2020517
Ching Lin <clin@bridgewell.com>
2021-04-14 00:57:39 +0800

    [feature] merge files and use itertools

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import csv
+import itertools
 
 history = []
-unigram_count = {}
-bigram_count = {}
-trigram_count = {}
-fourgram_count = {}
+ngram_count = {}
 
 def main():
     with open('history.csv') as csvfile:
@@ -14,37 +12,19 @@ def main():
             history.append(numeric_row)
             update_ngrams(numeric_row)
 
-    write_ngrams('unigram.csv', unigram_count)
-    write_ngrams('bigram.csv', bigram_count)
-    write_ngrams('trigram.csv', trigram_count)
-    write_ngrams('fourgram.csv', fourgram_count)
-
+    write_ngrams()
 
 def update_ngrams(row):
-    for i in range(5):
-        unigram = row[i]
-        c = unigram_count.get(unigram, 0)
-        unigram_count[unigram] = c + 1
-
-        for j in range(i + 1, 5):
-            bigram = (row[i], row[j])
-            c = bigram_count.get(bigram, 0)
-            bigram_count[bigram] = c + 1
-
-            for k in range(j + 1, 5):
-                trigram = (row[i], row[j], row[k])
-                c = trigram_count.get(trigram, 0)
-                trigram_count[trigram] = c + 1
-
-        fourgram = tuple(row[0:i] + row[i+1:5])
-        c = fourgram_count.get(fourgram, 0)
-        fourgram_count[fourgram] = c + 1
+    for n in range(1, 5):
+        for t in itertools.combinations(row, n):
+            c = ngram_count.get(t, 0)
+            ngram_count[t] = c + 1
 
 
-def write_ngrams(filename, counts):
-    with open(filename, 'w', newline='\n') as csvfile:
-        writer = csv.writer(csvfile, delimiter='\t')
-        for k, v in sorted(counts.items()):
+def write_ngrams():
+    with open('ngram.csv', 'w', newline='\n') as f:
+        writer = csv.writer(f, delimiter='\t')
+        for k, v in sorted(ngram_count.items(), key=lambda x : (len(x[0]), x)):
             writer.writerow([k, v])
 
 
